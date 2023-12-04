9ce986f01fa9e729fb7252dce01ffa16aab335bf
eduardobr33 <ebergillosrivas@gmail.com>
2023-12-04 08:49:04 +0100

    -

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -25,7 +25,7 @@ bool ModuleSceneIntro::Start()
 
 	App->renderer->camera.x = App->renderer->camera.y = 0;
 
-	circle = App->textures->Load("pinball/wheel.png"); 
+	//circle = App->textures->Load("pinball/wheel.png"); 
 	box = App->textures->Load("pinball/crate.png");
 	rick = App->textures->Load("pinball/rick_head.png");
 	bonus_fx = App->audio->LoadFx("pinball/bonus.wav");
